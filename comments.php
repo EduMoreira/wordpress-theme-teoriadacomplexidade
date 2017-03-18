250c007d0e9192c79f8b46276874939f3523b679
Unknown <du.junior@live.com>
2017-03-18 11:33:23 -0300

    Ocultando titulo de comentarios

--- comments.php
+++ comments.php
@@ -29,9 +29,7 @@ if ( post_password_required() ) {
 				}
 			?>
 		</h2>
-
 		<?php the_comments_navigation(); ?>
-
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
@@ -41,23 +39,18 @@ if ( post_password_required() ) {
 				) );
 			?>
 		</ol><!-- .comment-list -->
-
 		<?php the_comments_navigation(); ?>
-
 	<?php endif; // Check for have_comments(). ?>
-
 	<?php
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentysixteen' ); ?></p>
 	<?php endif; ?>
-
 	<?php
 		comment_form( array(
 			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
 			'title_reply_after'  => '</h2>',
 		) );
 	?>
-
 </div><!-- .comments-area -->
